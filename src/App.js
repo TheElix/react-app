661ae4e0c7bcd5050bec5e7842cfccdcf5f8cbd7
TheElix <ualxdima@gmail.com>
2018-12-04 18:07:31 +0200

    Completed Block 2 Vid 4.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Car from './Car/Car'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello MF!</h1>
-      </div>
-    );
-  }
+    render() {
+        const divStyle = {
+            'textAlign': 'center'
+        };
+
+        // const h1Style = {
+        //     'color': 'blue',
+        //     'fontSize': '20px'
+        // };
+
+        return (
+            <div style={divStyle}>
+                <h1>Hello MF!</h1>
+
+                <Car/>
+            </div>
+        );
+
+        // return React.createElement(
+        //     'div',
+        //     {
+        //         className: 'App'
+        //
+        //     },
+        //     React.createElement(
+        //         'h1',
+        //         {},
+        //         'Hello MF!'
+        //     ),
+        // );
+    }
 }
 
 export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// function car() {
+//     return (
+//         <h2>This is something...</h2>
+//     )
+// }
+
+// const car = () => {
+//     return (
+//         <div>This is something...</div>
+//     )
+// };
+
+// const car = () => (
+//     <div>
+//         This is something...
+//         <strong>test</strong>
+//     </div>
+// );
+
+export default () => (
+    <div>
+        This is something...
+        <strong>test</strong>
+    </div>
+);
+
+// export default car
